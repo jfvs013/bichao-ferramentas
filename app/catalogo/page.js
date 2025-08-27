7ee25e15d1982723f93095c659d671df5184f520
Jacson Francisco Viana Santos <jfvs@aluno.ifnmg.edu.br>
2025-08-27 15:09:36 -0300

    e

--- app/catalogo/page.js
+++ app/catalogo/page.js
@@ -1,7 +1,7 @@
 import { Suspense } from 'react';
-import CatalogoContent from './CatalogoContent';
-import Header from '@/components/blocks/Header';
-import Footer from '@/components/blocks/Footer';
+import CatalogoContent from './/catalogo/CatalogoContent';
+import Header from '@/components/layout/Header';
+import Footer from '@/components/layout/Footer';
 
 export default function CatalogoPage() {
   return (
