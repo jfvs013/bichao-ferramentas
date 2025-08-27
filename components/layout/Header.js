6fa91931164047be08c61c9d8cad80be5a44d75c
Jacson Francisco Viana Santos <jfvs@aluno.ifnmg.edu.br>
2025-08-27 15:18:38 -0300

    aa

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -164,12 +164,6 @@ export default function Header() {
                   </svg>
                   Minha Lista
                 </Button>
-                <Button variant="ghost" className="w-full justify-start">
-                  <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4m0 0L7 13m0 0l-1.5 1.5M7 13l1.5 1.5M17 21a2 2 0 100-4 2 2 0 000 4zM9 21a2 2 0 100-4 2 2 0 000 4z" />
-                  </svg>
-                  Carrinho
-                </Button>
               </div>
             </nav>
           </div>
