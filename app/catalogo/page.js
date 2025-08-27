bdcb9393de595dec0baa51b9b94ce0f350f43fc5
Jacson Francisco Viana Santos <jfvs@aluno.ifnmg.edu.br>
2025-08-27 15:04:46 -0300

    d

--- app/catalogo/page.js
+++ app/catalogo/page.js
@@ -1,16 +1,16 @@
-// catalogo/page.js
 import { Suspense } from 'react';
-import dynamic from 'next/dynamic';
-
-// Importe o componente que contém o useSearchParams de forma dinâmica
-const CatalogoContent = dynamic(() => import('./CatalogoContent'), {
-  ssr: false, // Isso desabilita a renderização no lado do servidor para este componente
-});
+import CatalogoContent from './CatalogoContent';
+import Header from '@/components/blocks/Header';
+import Footer from '@/components/blocks/Footer';
 
 export default function CatalogoPage() {
   return (
-    <Suspense fallback={<div>Carregando catálogo...</div>}>
-      <CatalogoContent />
-    </Suspense>
+    <>
+      <Header />
+      <Suspense fallback={<div>Carregando catálogo...</div>}>
+        <CatalogoContent />
+      </Suspense>
+      <Footer />
+    </>
   );
 }
