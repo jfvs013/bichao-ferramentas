7183a7cee206f3c4a4ac16dba440e480daab314b
Jacson Francisco Viana Santos <jfvs@aluno.ifnmg.edu.br>
2025-08-30 16:33:58 -0300

    Fix Menu Hamburguer mobile header

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,4 +1,3 @@
-// components/layout/Header.js
 'use client';
 
 import { useState } from 'react';
@@ -86,7 +85,7 @@ export default function Header() {
               onClick={() => setIsMenuOpen(!isMenuOpen)}
               className="lg:hidden p-2"
             >
-              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <svg className="w-6 h-6 text-secondary-orange" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
               </svg>
             </button>
